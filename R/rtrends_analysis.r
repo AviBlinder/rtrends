eaa91cf21f74b43b312e3c0e2b084f006a2b2737
Avi Blinder <aviblinder@gmail.com>
2016-12-02 22:29:42 +0200

    further changes

---
+++ R/rtrends_analysis.r
@@ -0,0 +1,7 @@
+#' @title CRAN Stats for single package
+#' @author Avi Blinder
+#' @description Download CRAN data related to several packages in a range of dates
+#' @param log_name  Starting date for downloading CRAN Repositories
+#' @export
+#' data(sample_log)
+#' run_models(ros1_ts)
