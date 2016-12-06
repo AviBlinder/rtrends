a6e8b5f905e28b2da1fee79d808c70d8aff9cf3a
Avi Blinder <aviblinder@gmail.com>
2016-12-06 22:15:32 +0200

    zero warnings - zero notes

--- R/rtrends_analysis.r
+++ R/rtrends_analysis.r
@@ -2,12 +2,14 @@
 #' @author Avi Blinder
 #' @description Merge CRAN log data with County names
 #' @param cran_log  dataftame containing CRAN log data
+#' @param countries Countries lookup table
 #' data(sample_log)
-#' cran_summary(sample_log)
+#' data(countries)
+#' cran_summary(sample_log,countries)
 #' @export
 
-cran_summary <- function(cran_log){
-  data(countries)
+cran_summary <- function(cran_log,countries){
+
   multiple_pack_Stats_full <- merge(cran_log,countries,by.x = "country",by.y="country_iso_code")
 
 }
